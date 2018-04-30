6d289cd86c25f9c5b81c8d2266db5994261650df
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-01 01:30:43 +0200

    update example

--- examples/mithril-view.js
+++ examples/mithril-view.js
@@ -1,3 +1,4 @@
+var i = 0;
 
 m("div",{class:"",id:"","anything":"value"},"Value")
 
@@ -5,7 +6,13 @@ m("div",{class:"",id:"","anything":"value"},"Value")
 
 
 m("div",{class:"",id:"","anything":"value"},[
-	m("h3","Молчик")
+	m("h3",i),
+	m("p","Полный лох")
 ])
+
+while(i < 200){
+	i++;
+}
+
 
 // Выведет блок div с классом "" и id "" с содержимым h3 Молчик
