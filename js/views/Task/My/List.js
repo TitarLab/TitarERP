8ab41c93468c6e95af0988bc4f96f2bb46941fe5
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-01 15:07:50 +0200

    Update my task list

---
+++ js/views/Task/My/List.js
@@ -0,0 +1,12 @@
+define(['mithril','controllers/Task','models/Task'], function(n,TaskController,Task){
+    var TaskMyListView = {
+        oninit: function(){
+
+        },
+        view : function(){
+
+        },
+    }
+    return TaskMyListView;
+
+});
