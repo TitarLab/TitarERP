1162b361df93900315a70386bcc39e1edf8dea52
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-01 15:30:55 +0200

    Task Controller
    Edit:
    TaskController
    TaskAPI

--- api/api.php
+++ api/api.php
@@ -120,24 +120,69 @@ if($data->model == "client"){
 			$report->result = $result[0]->maxId+1;
 			echo json_encode($report, JSON_UNESCAPED_UNICODE);
 	}else if($data->action == "add"){
-			$sql = "INSERT INTO project (id, firstname, lastname, phone, email) values ('".$data->project->id."', '".$data->project->firstname."', '".$data->project->lastname."', '".$data->project->phone."', '".$data->project->email."')";
-			$db->request($sql, false);
-			$report->code = 200;
-			$report->info = "Работник успешно добавлен!";
-			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+			// $sql = "INSERT INTO project (id, firstname, lastname, phone, email) values ('".$data->project->id."', '".$data->project->firstname."', '".$data->project->lastname."', '".$data->project->phone."', '".$data->project->email."')";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->info = "Работник успешно добавлен!";
+			// echo json_encode($report, JSON_UNESCAPED_UNICODE);
 	}else if($data->action == "save"){
-			$sql = "UPDATE client set firstname = '".$data->project->firstname."', lastname = '".$data->project->lastname."', photo = '".$data->project->photo."', email = '".$data->project->email."', phone = '".$data->project->phone."' where id = '".$data->project->id."'";
-			$db->request($sql, false);
-			$report->code = 200;
-			$report->info = "Работник успешно обновлён!";
-			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+			// $sql = "UPDATE client set firstname = '".$data->project->firstname."', lastname = '".$data->project->lastname."', photo = '".$data->project->photo."', email = '".$data->project->email."', phone = '".$data->project->phone."' where id = '".$data->project->id."'";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->info = "Работник успешно обновлён!";
+			// echo json_encode($report, JSON_UNESCAPED_UNICODE);
 	}else if($data->action == "remove"){
-			$sql = "DELETE from project where id = '".$data->id."'";
-			$db->request($sql, false);
+			// $sql = "DELETE from project where id = '".$data->id."'";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->result = $result[0]->maxId+1;
+			// $report->info = "Работник успешно удалён!";
+			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else{
+			$report->code = "Некорректное действие";
+			echo $report;
+	}
+}else if($data->model == "task"){
+	if($data->action == "get"){
+			$sql = "SELECT task.*, task_status.name as "status" from task left join task_status on task_status.id = task.status_id where 1 order by id ";
+			$result = $db->request($sql);
+			$report->code = 200;
+			$report->result = $result;
+			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else if($data->action == "getCurrent"){
+			$sql = "SELECT task.*, task_status.name as "status" from task left join task_status on task_status.id = task.status_id where id = '".$data->id."' order by id ";
+			$result = $db->request($sql);
+			$project = $result[0];
+			//$sql = "SELECT id, name from project where client_id = '".$data->id."' order by id ";
+			//$client->projects = $db->request($sql);
+			$report->code = 200;
+			$report->result = $project;
+			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else if($data->action == "getNextId"){
+			$sql = "SELECT max(id) as 'maxId' from task where 1";
+			$result = $db->request($sql);
 			$report->code = 200;
 			$report->result = $result[0]->maxId+1;
-			$report->info = "Работник успешно удалён!";
 			echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else if($data->action == "add"){
+			// $sql = "INSERT INTO project (id, firstname, lastname, phone, email) values ('".$data->project->id."', '".$data->project->firstname."', '".$data->project->lastname."', '".$data->project->phone."', '".$data->project->email."')";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->info = "Работник успешно добавлен!";
+			// echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else if($data->action == "save"){
+			// $sql = "UPDATE client set firstname = '".$data->project->firstname."', lastname = '".$data->project->lastname."', photo = '".$data->project->photo."', email = '".$data->project->email."', phone = '".$data->project->phone."' where id = '".$data->project->id."'";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->info = "Работник успешно обновлён!";
+			// echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	}else if($data->action == "remove"){
+			// $sql = "DELETE from project where id = '".$data->id."'";
+			// $db->request($sql, false);
+			// $report->code = 200;
+			// $report->result = $result[0]->maxId+1;
+			// $report->info = "Работник успешно удалён!";
+			// echo json_encode($report, JSON_UNESCAPED_UNICODE);
 	}else{
 			$report->code = "Некорректное действие";
 			echo $report;
--- js/controllers/Task.js
+++ js/controllers/Task.js
@@ -1,11 +1,124 @@
 define(['mithril','titar','models/Task'], function(n,t,Task){
 
     var TaskController = {
-        init:{
-            default:function(){
+			init:{
+					default:function(){
+						TaskController.clearCurrent();
+							TaskController.load.list();
+					},
+					new:function(){
+						TaskController.init.default();
+						TaskController.load.nextId();
+					},
+					current:function(id){
+						TaskController.init.default();
+						TaskController.load.current(id);
+						Task.list.forEach(function(item){
+							if(item.id == id){
+								Task.current = item;
+							}
+						})
 
-            }
-        }
+
+					}
+			},
+				load:{
+            list:function(){
+                m.request({
+                    method: "POST",
+                    url:"../api/api.php",
+                    data:{model:"task",action:"get"},
+                    withCredentials:true,
+                }).then(function(report){
+                    if(report.code == 200){
+                        Task.list = report.result;
+                    }else{
+
+                    }
+                });
+            },
+						current:function(id){
+							m.request({
+									method: "POST",
+									url:"../api/api.php",
+									data:{model:"task",action:"getCurrent", id:id},
+									withCredentials:true,
+							}).then(function(report){
+									if(report.code == 200){
+											Task.current = report.result
+									}else{
+
+									}
+							});
+						},
+						nextId:function(){
+							m.request({
+									method: "POST",
+									url:"../api/api.php",
+									data:{model:"task",action:"getNextId"},
+									withCredentials:true,
+							}).then(function(report){
+									if(report.code == 200){
+											Task.current.id = report.result;
+									}else{
+
+									}
+							});
+						},
+						add:function(){
+							m.request({
+									method: "POST",
+									url:"../api/api.php",
+									data:{model:"task",action:"add", task:Task.current},
+									withCredentials:true,
+							}).then(function(report){
+									if(report.code == 200){
+										UIkit.notification("<span uk-icon='icon: check'></span>"+report.info,{status:'success'});
+									}else{
+
+									}
+							});
+						},
+						save:function(){
+							if(Project.current != null){
+								m.request({
+										method: "POST",
+										url:"../api/api.php",
+										data:{model:"task",action:"save", task:Task.current},
+										withCredentials:true,
+								}).then(function(report){
+										if(report.code == 200){
+											UIkit.notification("<span uk-icon='icon: check'></span>"+report.info,{status:'success'});
+										}else{
+
+										}
+								});
+							}
+
+						},
+						remove:function(id){
+							if(id != null){
+								m.request({
+										method: "POST",
+										url:"../api/api.php",
+										data:{model:"task",action:"remove", id:id},
+										withCredentials:true,
+								}).then(function(report){
+										if(report.code == 200){
+											UIkit.notification("<span uk-icon='icon: check'></span>"+report.info,{status:'success'});
+										}else{
+
+										}
+								});
+							}
+
+						},
+						clearCurrent:function(){
+							Object.keys(Task.current).forEach(function(item){
+								Task.current[item] = null;
+							});
+						}
+        },
     }
 
     return TaskController;
