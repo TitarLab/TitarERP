834ea7c537cb3bf333d4ac0db0f693089de44995
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-01 15:58:03 +0200

    fix MyTask

--- js/views/Task/My/List.js
+++ js/views/Task/My/List.js
@@ -7,7 +7,7 @@ define(['mithril','controllers/Task','models/Task'], function(n,TaskController,T
             return[
                 m("div.uk-flex uk-flex-column uk-width-expand",[
                     m("div.uk-flex uk-flex-between uk-flex-middle uk-width-1-1 uk-padding-small t-border-bottom",[
-                        m("h2.uk-margin-remove","Мои задачи"),
+                        m("h3.uk-margin-remove","Мои задачи"),
                         m("div.uk-flex uk-flex-right",[
                             m("div.uk-button-group",[
                                 m("div.uk-search uk-search-default",[
