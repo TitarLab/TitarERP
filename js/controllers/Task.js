7370785a2fe4a3fe5bd012562b309069614ac509
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-01 15:17:39 +0200

    Task structure
    + Controller
    + Model

---
+++ js/controllers/Task.js
@@ -0,0 +1,12 @@
+define(['mithril','titar','models/Task'], function(n,t,Task){
+
+    var TaskController = {
+        init:{
+            default:function(){
+
+            }
+        }
+    }
+
+    return TaskController;
+});
---
+++ js/models/Task.js
@@ -0,0 +1,12 @@
+define(['mithril','titar'], function(n,t){
+
+    var Task = {
+        list:[],
+				current:{
+					id:0,
+					name:"",
+				}
+    }
+
+    return Task;
+});
