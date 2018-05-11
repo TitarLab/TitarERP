e623aeae836a0b714f126a7d111570ab2077c1cf
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-11 20:20:07 +0200

    Medoo integrate

--- api/controllers/client.php
+++ api/controllers/client.php
@@ -22,24 +22,68 @@ if($data->action == "get"){
 		$report->result = $result[0]->maxId+1;
 		echo json_encode($report, JSON_UNESCAPED_UNICODE);
 }else if($data->action == "add"){
-		$sql = "INSERT INTO client (id, firstname, lastname, status, note, last_contact, contacts) values ('".$data->client->id."', '".$data->client->firstname."', '".$data->client->lastname."', '".$data->client->status."', '".$data->client->note."', '".$data->client->lastContact."', '".json_encode($data->client->contacts,JSON_UNESCAPED_UNICODE)."')";
-		$db->request($sql, false);
-		$report->code = 200;
-		$report->info = "Клиент успешно добавлен!";
-		echo json_encode($report, JSON_UNESCAPED_UNICODE);
+
+
+	require '../libs/Medoo.php';
+	use Medoo\Medoo;
+
+
+	$db = new Medoo([
+	    'database_type' => 'mysql',
+	    'database_name' => 'rivaldi_titarcrm',
+	    'server' => 'rivaldi.mysql.tools',
+	    'username' => 'rivaldi_titarcrm',
+	    'password' => 'aqqdevje'
+	]);
+
+	$data = $db->select("client",[
+		"firstname",
+		"lastname"
+	],[
+		"id" => 1
+	]);
+
+	echo json_encode($data);
+
+
+
+
+	$db->insert("client", [
+		"id" => $data->client->id,
+		"firstname" => $data->client->firstname,
+		"lastname" => $data->client->lastname,
+		"status" => $data->client->status,
+		"note" => $data->client->note,
+		"last_contact" => $data->client->lastContact,
+		"contacts[JSON]" => $data->client->contacts
+	]);
+	$report->code = 200;
+	$report->info = "Клиент успешно добавлен!";
+	echo json_encode($report, JSON_UNESCAPED_UNICODE);
 }else if($data->action == "save"){
-		$sql = "UPDATE client set firstname = '".$data->client->firstname."', lastname = '".$data->client->lastname."', status = '".$data->client->status."', note = '".$data->client->note."', last_contact = '".$data->client->lastContact."', contacts = '".json_encode($data->client->client->contacts,JSON_UNESCAPED_UNICODE)."', photo = '".$data->client->photo."', email = '".$data->client->email."', phone = '".$data->client->phone."' where id = '".$data->client->id."'";
-		$db->request($sql, false);
-		$report->code = 200;
-		$report->info = "Клиент успешно обновлён!";
-		echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	$db->update("account", [
+		"firstname" => $data->client->firstname,
+		"lastname" => $data->client->lastname,
+		"status" => $data->client->status,
+		"note" => $data->client->note,
+		"last_contact" => $data->client->lastContact,
+		"contacts[JSON]" => $data->client->contacts,
+		"photo" => $data->client->photo,
+		"email" => $data->client->email,
+		"phone" => $data->client->phone
+	], [
+		"id" => $data->client->id
+	]);
+	$report->code = 200;
+	$report->info = "Клиент успешно обновлён!";
+	echo json_encode($report, JSON_UNESCAPED_UNICODE);
 }else if($data->action == "remove"){
-		$sql = "DELETE from client where id = '".$data->id."'";
-		$db->request($sql, false);
-		$report->code = 200;
-		$report->result = $result[0]->maxId+1;
-		$report->info = "Клиент успешно удалён!";
-		echo json_encode($report, JSON_UNESCAPED_UNICODE);
+	$db->delete("client", [
+		"id" => $data->id
+	]);
+	$report->code = 200;
+	$report->info = "Клиент успешно удалён!";
+	echo json_encode($report, JSON_UNESCAPED_UNICODE);
 }else{
 		$report->code = "Некорректное действие";
 		echo $report;
