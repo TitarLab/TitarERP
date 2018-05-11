82aa371a8372873215753cf0a85e6275d7f4217e
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-11 20:50:43 +0200

    Ebalis s medoo

--- installation/installator.php
+++ installation/installator.php
@@ -1,7 +1,7 @@
 <?php
 //require_once("../api/report.php");
-$postData = file_get_contents('php://input');
-$data = json_decode($postData);
+// $postData = file_get_contents('php://input');
+// $data = json_decode($postData);
 //$report = new report;
 
 require '../libs/Medoo.php';
@@ -12,14 +12,11 @@ $db = new Medoo([
     'database_name' => 'rivaldi_titarcrm',
     'server' => 'rivaldi.mysql.tools',
     'username' => 'rivaldi_titarcrm',
-    'password' => 'aqqdevje'
+    'password' => 'aqqdevje',
+		'charset' => 'utf8',
 ]);
 
-$data = $db->select("client",[
-  "firstname",
-  "lastname"
-],[
-  "id" => 1
-]);
 
-echo json_encode($data);
+
+$datas = $db->select("client", "*");
+echo json_encode($datas);
