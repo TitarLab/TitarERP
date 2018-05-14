238638d4344450725a31e840aceb6c3f088c32e5
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-05-14 13:34:28 +0200

    Medoo

--- api/controllers/project.php
+++ api/controllers/project.php
@@ -145,25 +145,48 @@ if($data->action == "get"){
 
 }else if($data->action == "addCategory"){
 		$id = -1;
-		$sql = "SELECT id, name from task_category where name = '".$data->value."'";
-		$result = $db->request($sql);
+		$result = $db->select("task_category",[
+			"id",
+			"name",
+		],[
+			"ORDER" => ["id" => "ASC"],
+			"name" => $data->value
+		]);
+		$result = json_decode(json_encode($result));
 		if(empty($result) != 1){
 			$id = $result[0]->id;
 		}else if(empty($result) == 1) {
-			$sql = "INSERT INTO task_category (name) values ('".$data->value."')";
-			$db->request($sql,false);
-			$sql = "SELECT id, name from task_category where name = '".$data->value."'";
-			$result = $db->request($sql);
+			$db->insert("task_category", [
+				"name" => $data->value,
+			]);
+			$result = $db->select("task_category",[
+				"id",
+				"name",
+			],[
+				"ORDER" => ["id" => "ASC"],
+				"name" => $data->value
+			]);
+			$result = json_decode(json_encode($result));
 			$id = $result[0]->id;
 		}
 		$categoryId = 1;
-		$sql = "SELECT max(id) as 'max' from project_task_category where 1";
-		$result = $db->request($sql);
-		$categoryId = $result[0]->max+1;
-		$sql = "INSERT INTO project_task_category (id, id_project, id_task_category) values ('".$categoryId."', '".$data->id."', '".$id."')";
-		$db->request($sql,false);
-		$sql = "SELECT task_category.name from project_task_category left join task_category on task_category.id = id_task_category where project_task_category.id = '".$categoryId."'";
-		$result = $db->request($sql);
+		$result = $db->max("project_task_category","id");
+		$categoryId = $result+1;
+		$db->insert("project_task_category", [
+			"id" => $categoryId,
+			"id_project" => $data->id,
+			"id_task_category" => $id,
+		]);
+		//echo json_encode($db->error());
+		$result = $db->select("project_task_category",[
+			"[>]task_category" => ["id_task_category" => "id"]
+		],[
+			"task_category.name",
+		],[
+			"project_task_category.id" => $categoryId
+		]);
+		//echo json_encode($db->error());
+		$result = json_decode(json_encode($result));
 		$report->code = 200;
 		$report->result = $result[0];
 		$report->info = "Категория успешно добавлена";
