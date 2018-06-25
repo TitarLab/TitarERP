e26fe0eac0c18266e5ce34926e3dd3e1dcdc8f45
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-06-26 00:42:07 +0200

    Still not best solution for menu

--- js/models/Menu.js
+++ js/models/Menu.js
@@ -9,12 +9,10 @@ define(['mithril','titar','models/Auth'], function(n,t,Auth){
             {href:"#!/task/my",name:t.localisation.dictionary.MY_TASKS,onclick:function(){}},
         ],
 		reload:()=>{
-			this.pages = pages:[
-	            {href:"#!/client/list",name:t.localisation.dictionary.CLIENTS,onclick:function(){}},
-	            {href:"#!/employee/list",name:t.localisation.dictionary.EMPLOYEES,onclick:function(){}},
-	            {href:"#!/project/list",name:t.localisation.dictionary.PROJECTS,onclick:function(){}},
-	            {href:"#!/task/my",name:t.localisation.dictionary.MY_TASKS,onclick:function(){}},
-	        ]
+			Menu.pages[0].name = t.localisation.dictionary.CLIENTS;
+			Menu.pages[1].name = t.localisation.dictionary.EMPLOYEES;
+			Menu.pages[2].name = t.localisation.dictionary.PROJECTS;
+			Menu.pages[3].name = t.localisation.dictionary.MY_TASKS;
 		}
     }
 
