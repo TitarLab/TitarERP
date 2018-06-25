c9b3a02a30d76af308eb88c559b9776f54a34d28
Volodymyr Tytarenko <bovatitar@gmail.com>
2018-06-26 00:46:23 +0200

    fix EditTask local

--- js/views/Project/Modal/EditTask.js
+++ js/views/Project/Modal/EditTask.js
@@ -68,7 +68,7 @@ define(['mithril','titar','controllers/Project','controllers/Task','controllers/
 										// 	])
 										// ]),
 										m("div.uk-margin",[
-											m("button.uk-button uk-button-primary uk-modal-close",{onclick:function(){TaskController.add()}},t.localisation.dictionary.ADD)
+											m("button.uk-button uk-button-primary uk-modal-close",{onclick:function(){TaskController.add()}},t.localisation.dictionary.SAVE)
 										]),
 									])
 								])
